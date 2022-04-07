5822d15de48f221281f1d247e2443ba34b97a50d
JianouShunYan <103194144+JianouShunYan@users.noreply.github.com>
2022-04-07 23:20:13 +0800

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,5 +3,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("nihaoma ");
         System.out.println("nihaoma 2 ");
+        System.out.println("nihaoma pulltest ");
     }
 }
